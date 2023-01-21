5ff5c941250fc6db01e7e936676f3f86d515c3e3
27794 <2779435956@qq.com>
2023-01-21 18:51:45 +0800

    doc[doc]:v1.0

--- sprite/input单例与自定义单例.gd
+++ sprite/input单例与自定义单例.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+var map_position:Vector2
+var icon_speed:int = 10
+
 func _ready():
 	print(arr_and_for.a)
 	pass # Replace with function body.
@@ -7,13 +10,40 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("ui_left"):
-		self.position.x -= 1
-	elif Input.is_action_pressed("ui_right"):
-		self.position.x += 1
-	elif Input.get_mouse_button_mask()==MOUSE_BUTTON_LEFT:
-		print(Input.get_last_mouse_velocity())
-		self.position.y += 1
+
+#	if Input.is_action_pressed("ui_left"):
+#		self.position.x -= icon_speed
+#	if Input.is_action_pressed("ui_right"):
+#		self.position.x += icon_speed
+#	if Input.is_action_pressed("ui_down"):
+#		self.position.y += icon_speed
+#	if Input.is_action_pressed("ui_up"):
+#		self.position.y -= icon_speed
+	if map_position.y > $Icon.position.y:
+		$Icon.position.y += icon_speed
+	if map_position.y < $Icon.position.y:
+		$Icon.position.y -= icon_speed
+	if map_position.x > $Icon.position.x:
+		$Icon.position.x += icon_speed
+	if map_position.x < $Icon.position.x:
+		$Icon.position.x -= icon_speed
 	else:
 		return
+	
+	
 	pass
+
+func _input(event):
+   # Mouse in viewport coordinates.
+	if event is InputEventMouseButton:
+		print(event.position)
+		map_position = event.position
+#		if event.position.x > $Icon.position.x:
+#			$Icon.position.x += 1
+#		elif event.position.x < $Icon.position.x:
+#			$Icon.position.x -= 1
+#		else:
+#			return	
+		
+
+
